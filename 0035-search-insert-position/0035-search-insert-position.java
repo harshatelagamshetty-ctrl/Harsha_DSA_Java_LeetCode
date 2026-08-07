705cc0df9b9b9e36cb3c1cf66c0c5414159c06b6
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-08-07 23:20:33 +0530

    Time: 0 ms (100%), Space: 44.7 MB (67.22%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] arr, int target) {
+        int i=0;
+        int j=arr.length-1;
+        int idx=-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(arr[mid]<target){
+                idx=mid;
+                i=mid+1;
+            }
+            else if(arr[mid]>=target){
+                j=mid-1;
+            }
+        }
+        return idx+1;
+    }
+}
