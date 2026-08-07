cf9cb99abb6a72a5e8ea6521d2e05001dde5a91c
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-08-07 14:25:56 +0530

    Time: 0 ms (100%), Space: 43.5 MB (43.8%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -1,25 +1,38 @@
 class Solution {
     public void sortColors(int[] arr) {
-        int count0=0;
-        int count1=0;
-        int count2=0;
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]==0) count0++;
-            else if(arr[i]==1) count1++;
-            else if(arr[i]==2) count2++;
+        int i=0;
+        int j=arr.length-1;
+        while(i<=j){
+            if(arr[i]==0) i++;
+            else if (arr[j]==0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+                i++;
+                j--;
+            }
+            else j--;
         }
         int idx=0;
-        for(int  i=1;i<=count0;i++){
-            arr[idx]=0;
-            idx++;
+        for(int a=0;a<arr.length;a++){
+            if(arr[a]!=0){
+                idx=a;
+                break;
+            }
         }
-        for(int  i=1;i<=count1;i++){
-            arr[idx]=1;
-            idx++;
+        int a=idx;
+        int b=arr.length-1;
+        while(a<=b){
+            if(arr[a]==1) a++;
+            else if(arr[b]==1){
+                int temp=arr[a];
+                arr[a]=arr[b];
+                arr[b]=temp;
+                a++;
+                b--;
+            }
+            else b--;
         }
-        for(int  i=1;i<=count2;i++){
-            arr[idx]=2;
-            idx++;
-        }
+        return;
     }
 }
