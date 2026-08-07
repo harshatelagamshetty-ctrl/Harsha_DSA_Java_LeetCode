508d0739c40817afcd4aa8071a0faff5f1a098f1
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-08-07 16:39:53 +0530

    Time: 1 ms (77.27%), Space: 47.1 MB (9.49%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -3,15 +3,17 @@ class Solution {
         int i=0;
         int j=0;
         ArrayList<Integer> list=new ArrayList<>();
-        while(i<arr.length){
-            while(j<arr.length && arr[i]==arr[j]){
-                j++;
+        while(j<arr.length){
+            if(arr[i]==arr[j]) j++;
+            else{
+                int freq=j-i;
+                if(freq>=1) list.add(arr[i]);
+                i = j;
             }
-            int freq=j-i;
-            if(freq>=1) list.add(arr[i]);
-            
-            i=j;
         }
+        int freq=j-i;
+        if(freq>=1) list.add(arr[i]);
+
         for(int a=0;a<list.size();a++){
             arr[a]=list.get(a);
         }
