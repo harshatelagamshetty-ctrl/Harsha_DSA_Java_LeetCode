1aa755372db0caa702568230f518dc56d2b855ae
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-08-09 14:42:46 +0530

    Time: 0 ms (100%), Space: 45.3 MB (12.82%) - LeetHub

---
+++ 0081-search-in-rotated-sorted-array-ii/0081-search-in-rotated-sorted-array-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public boolean search(int[] arr, int target) {
+        int p=findPivotIdx(arr);
+        int i=0;
+        int j=p-1;
+        while(i<=j){
+            int mid=i+(j-i)/2;
+            if(arr[mid]==target) return true;
+            else if(arr[mid]>target) j=mid-1;
+            else i=mid+1;
+        }
+
+        int a=p;
+        int b=arr.length-1;
+        while(a<=b){
+            int mid=a+(b-a)/2;
+            if(arr[mid]==target) return true;
+            else if(arr[mid]>target) b=mid-1;
+            else a=mid+1;
+        }
+        return false;
+    }
+    public int findPivotIdx(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]) return i+1;
+        }
+        return 0;
+    }
+}
