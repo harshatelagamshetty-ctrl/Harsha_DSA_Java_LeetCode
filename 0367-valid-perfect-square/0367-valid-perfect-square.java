4bc1a39275604816e0d8f97ec8427d4a2e8d01b5
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-08-11 01:31:22 +0530

    Time: 0 ms (100%), Space: 42.2 MB (19.86%) - LeetHub

--- 0367-valid-perfect-square/0367-valid-perfect-square.java
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -1,13 +1,15 @@
 class Solution {
-    public boolean isPerfectSquare(int n) {
+    public boolean isPerfectSquare(int x) {
         long i=1;
-        long j=n;
+        long j=x;
         long idx=0;
         while(i<=j){
             long mid=i+(j-i)/2;
-            if((long)mid*mid==n) return true;
-            else if((long)mid*mid<n) i=mid+1;
-            else j=mid-1;   
+            if((long)mid*mid==x) return true;
+            else if((long)mid*mid<x){
+                i=mid+1;
+            }
+            else if((long)mid*mid>x) j=mid-1;
         }
         return false;
     }
