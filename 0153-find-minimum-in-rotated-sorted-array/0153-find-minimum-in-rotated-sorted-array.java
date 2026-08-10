ad379f5f330d81077593be48a82c5b8566a7cb00
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-08-10 23:45:50 +0530

    Time: 0 ms (100%), Space: 43.8 MB (38.49%) - LeetHub

--- 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
+++ 0153-find-minimum-in-rotated-sorted-array/0153-find-minimum-in-rotated-sorted-array.java
@@ -1,6 +1,12 @@
 class Solution {
     public int findMin(int[] arr) {
-        Arrays.sort(arr);
-        return arr[0];
+        int p=pivotIdx(arr);
+        return Math.min(arr[0],arr[p]);
+    }
+    public int pivotIdx(int[] arr){
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]) return i+1;
+        }
+        return 0;
     }
 }
