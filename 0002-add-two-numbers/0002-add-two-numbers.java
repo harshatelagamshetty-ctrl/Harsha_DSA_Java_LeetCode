e7a873019ed3bd8a2f29d3646546e7d85f3b32a8
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-08-12 14:27:08 +0530

    Time: 1 ms (100%), Space: 46.2 MB (95.64%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.java
+++ 0002-add-two-numbers/0002-add-two-numbers.java
@@ -1,3 +1,13 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode dummy=new ListNode(-1);
@@ -6,62 +16,59 @@ class Solution {
         ListNode j=l2;
         int carry=0;
         while(i!=null && j!=null){
-
-        if(i.val+j.val+carry<=9){
-            ListNode l=new ListNode(i.val+j.val+carry);
-            temp.next=l;
-            temp=temp.next;
-            carry=0;
-        }
-        else{
             int sum=i.val+j.val+carry;
-            ListNode l=new ListNode(sum%10);
-            temp.next=l;
-            temp=temp.next;
-            carry=1;
+            if(sum<=9){
+                ListNode l=new ListNode(sum);
+                carry=0;
+                temp.next=l;
+                temp=temp.next;
+            }
+            else{
+                ListNode l=new ListNode(sum%10);
+                carry=1;
+                temp.next=l;
+                temp=temp.next;
+            }
+            i=i.next;
+            j=j.next;
         }
-        i=i.next;
-        j=j.next;
-    }
-    while(i!=null){
-        int sum=i.val+carry;
-        if(sum<=9){
-            ListNode l=new ListNode(sum);
-            temp.next=l;
-            temp=temp.next;
-            carry=0;
+        while(i!=null){
+            int sum=i.val+carry;
+            if(sum<=9){
+                ListNode l=new ListNode(sum);
+                carry=0;
+                temp.next=l;
+                temp=temp.next;
+            }
+            else{
+                ListNode l=new ListNode(sum%10);
+                carry=1;
+                temp.next=l;
+                temp=temp.next;
+            }
             i=i.next;
         }
-        else{
-            ListNode l=new ListNode(sum%10);
-            temp.next=l;
-            temp=temp.next;
-            carry=1;
-            i=i.next;
+        while(j!=null){
+            int sum=j.val+carry;
+            if(sum<=9){
+                ListNode l=new ListNode(sum);
+                carry=0;
+                temp.next=l;
+                temp=temp.next;
+            }
+            else{
+                ListNode l=new ListNode(sum%10);
+                carry=1;
+                temp.next=l;
+                temp=temp.next;
+            }
+            j=j.next;
         }
-    }
 
-    while(j!=null){
-        int sum=j.val+carry;
-        if(sum<=9){
-            ListNode l=new ListNode(sum);
+        if(carry==1){
+            ListNode l=new ListNode(1);
             temp.next=l;
-            temp=temp.next;
-            carry=0;
-            j=j.next;
         }
-        else{
-            ListNode l=new ListNode(sum%10);
-            temp.next=l;
-            temp=temp.next;
-            carry=1;
-            j=j.next;
-        }
-    }
-    if(carry==1){
-        ListNode l=new ListNode(1);
-        temp.next=l;
-    }
         return dummy.next;
     }
 }
