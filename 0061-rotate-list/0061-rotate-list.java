a914f3a30ac3d96c8cbac2887da57742eeeab680
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-08-12 16:18:37 +0530

    Time: 0 ms (100%), Space: 44.3 MB (63.33%) - LeetHub

--- 0061-rotate-list/0061-rotate-list.java
+++ 0061-rotate-list/0061-rotate-list.java
@@ -21,8 +21,8 @@ class Solution {
             len++;
         }
         x=head;
-        if(k==len) return head;
         k=k%len;
+        if(k==len) return head;
         if(k==0) return head;
         for(int i=1;i<=k;i++){
             f=f.next;
