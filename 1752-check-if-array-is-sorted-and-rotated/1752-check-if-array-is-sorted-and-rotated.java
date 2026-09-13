5605731da4ca6d78ef48f1468c65f5b9b4a25899
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-09-14 00:01:37 +0530

    Time: 1 ms (6.03%), Space: 42.9 MB (81.53%) - LeetHub

---
+++ 1752-check-if-array-is-sorted-and-rotated/1752-check-if-array-is-sorted-and-rotated.java
@@ -0,0 +1,26 @@
+class Solution {
+    public boolean check(int[] arr) {
+        int pivotIdx=-1;
+        ArrayList<Integer>list=new ArrayList<>();
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]>arr[i+1]){
+                pivotIdx=i;
+                break;
+            }
+        }
+        if(pivotIdx==-1) return true;
+        for(int i=pivotIdx+1;i<arr.length;i++){
+            list.add(arr[i]);
+        }
+        for(int i=0;i<=pivotIdx;i++) list.add(arr[i]);
+        boolean sorted = true;
+
+        for (int i=0; i<list.size()-1;i++) {
+            if(list.get(i)>list.get(i+1)) {
+                sorted = false;
+                return false;
+            }
+        }
+        return true;
+    }
+}
