c5fb5615e9da7352ce2b68627b2ec7c3bb780bd7
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-09-15 00:25:04 +0530

    Time: 29 ms (78.28%), Space: 77.1 MB (98.23%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -19,16 +19,13 @@ class Solution {
         return Collections.max(list);
     }
     public int[] sortAndRemoveDuplicates(int[] arr){
-                Arrays.sort(arr);
-
-        int j = 0;
-
-        for (int i = 1; i < arr.length; i++) {
-            if (arr[i] != arr[j]) {
-                arr[++j] = arr[i];
-            }
+        Arrays.sort(arr);
+        ArrayList<Integer>list=new ArrayList<>();
+        for(int i=0;i<arr.length-1;i++){
+            if(arr[i]!=arr[i+1]) list.add(arr[i]);
         }
-
-        return Arrays.copyOf(arr, j + 1);
+        list.add(arr[arr.length-1]);
+        for(int i=0;i<list.size();i++) arr[i]=list.get(i);
+        return arr;
     }
 }
