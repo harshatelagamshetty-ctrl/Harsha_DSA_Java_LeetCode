1f4b5559cf042a92f1f2f03f52eff71ecc222ccf
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-09-14 14:47:41 +0530

    Time: 0 ms (100%), Space: 44 MB (30.32%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -1,43 +1,18 @@
 class Solution {
     public void merge(int[] arr, int m, int[] brr, int n) {
+        ArrayList<Integer> list=new ArrayList<>();
         int i=0;
         int j=0;
-        int k=0;
-        int[] res=new int[arr.length];
-        while(i<m && j<n){
-            if(arr[i]<brr[j]) {
-                res[k]=arr[i];
-                i++;
-                k++;
+        while(i<m&&j<n){
+            if(arr[i]<brr[j]){
+                list.add(arr[i++]);
             }
-            else if(arr[i]>brr[j]){
-                res[k]=brr[j];
-                j++;
-                k++;
-            }
-            else if (arr[i]==brr[j]){
-                res[k]=arr[i];
-                i++;
-                k++;
-                res[k]=brr[j];
-                k++;
-                j++;
+            else{
+                list.add(brr[j++]);
             }
         }
-
-        while(i<m){
-            res[k]=arr[i];
-            i++;
-            k++;
-        }
-        while(j<n){
-            res[k]=brr[j];
-            j++;
-            k++;
-        }
-
-        for(int a=0;a<arr.length;a++){
-            arr[a]=res[a];
-        }
+        while(i<m) list.add(arr[i++]);
+        while(j<n) list.add(brr[j++]);
+        for(int a=0;a<list.size();a++) arr[a]=list.get(a);
     }
 }
