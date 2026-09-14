8cd117ad12504e1a6bc78e5b02ff28c91bdae715
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-09-14 14:56:18 +0530

    Time: 10 ms (7.75%), Space: 47.9 MB (10.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,22 +1,21 @@
 class Solution {
-    public int missingNumber(int[] arr){
-        Arrays.sort(arr);
-        List<Pair>list=new ArrayList<>();
-        for(int i=0;i<arr.length;i++){
-            list.add(new Pair(arr[i],i));
-        }
-        for(int i=0;i<list.size();i++){
-            if(list.get(i).val!=list.get(i).idx) return list.get(i).idx;
-        }
-        return arr.length;
-
-    }
-    class Pair{
+    static class Pair{
         int val;
         int idx;
         Pair(int val,int idx){
             this.val=val;
             this.idx=idx;
         }
+    }
+    public int missingNumber(int[] arr) {
+        Arrays.sort(arr);
+        ArrayList<Pair>list=new ArrayList<>();
+        for(int i=0;i<arr.length;i++){
+            list.add(new Pair(arr[i],i));
+        }
+        for(int i=0;i<list.size();i++){
+            if(list.get(i).val!=list.get(i).idx) return i;
+        }
+        return arr.length;
     }
 }
