549b089bf7a97342524eaad38e2d758831a63602
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-09-14 14:11:55 +0530

    Time: 0 ms (100%), Space: 43.4 MB (58.35%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.java
+++ 0075-sort-colors/0075-sort-colors.java
@@ -3,36 +3,43 @@ class Solution {
         int i=0;
         int j=arr.length-1;
         while(i<=j){
-            if(arr[i]==0) i++;
-            else if (arr[j]==0){
-                int temp=arr[i];
-                arr[i]=arr[j];
-                arr[j]=temp;
+            if(arr[i]==0 && arr[j]!=0){
                 i++;
                 j--;
             }
-            else j--;
-        }
-        int idx=0;
-        for(int a=0;a<arr.length;a++){
-            if(arr[a]!=0){
-                idx=a;
-                break;
+            else if(arr[i]==0 && arr[j]==0) i++;
+            else if(arr[i]!=0 && arr[j]==0){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+            else if(arr[i]!=0 && arr[j]!=0){
+                j--;
             }
         }
+        int idx = 0;
+
+        while (idx < arr.length && arr[idx] == 0) {
+            idx++;
+        }
+
+        
         int a=idx;
         int b=arr.length-1;
         while(a<=b){
-            if(arr[a]==1) a++;
-            else if(arr[b]==1){
-                int temp=arr[a];
-                arr[a]=arr[b];
-                arr[b]=temp;
+            if(arr[a]==1 && arr[b]!=1){
                 a++;
                 b--;
             }
-            else b--;
+            else if(arr[a]==1 && arr[b]==1) a++;
+            else if(arr[a]!=1 && arr[b]==1){
+                int temp=arr[a];
+                arr[a]=arr[b];
+                arr[b]=temp;
+            }
+            else if(arr[a]!=1 && arr[b]!=1){
+                b--;
+            }
         }
-        return;
     }
 }
