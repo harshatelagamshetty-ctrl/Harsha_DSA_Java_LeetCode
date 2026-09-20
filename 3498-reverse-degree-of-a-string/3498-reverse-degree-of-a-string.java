b0f3841430ea3e29f103dea36f95291b83207d9d
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-09-21 00:32:43 +0530

    Time: 1 ms (100%), Space: 44.2 MB (18.31%) - LeetHub

--- 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
+++ 3498-reverse-degree-of-a-string/3498-reverse-degree-of-a-string.java
@@ -1,16 +1,15 @@
 class Solution {
     public int reverseDegree(String s) {
         s=s.trim();
+        int sum=0;
         ArrayList<Integer> list=new ArrayList<>();
         for(int i=0;i<s.length();i++){
             char ch=s.charAt(i);
             int original=ch-'a'+1;
             int reversed=27-original;
             int idx=i+1;
-            list.add(reversed*idx);
+            sum=sum+reversed*idx;
         }
-        int sum=0;
-        for(int i=0;i<list.size();i++) sum=sum+list.get(i);
         return sum;
     }
 }
