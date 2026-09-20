154c5827b276aadefe85a3dc3571950f53e82c9c
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-09-20 23:45:13 +0530

    Time: 6 ms (34.45%), Space: 45 MB (38.69%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,16 +1,24 @@
 class Solution {
-    public boolean isPalindrome(String str) {
-        String s="";
-        for(int i=0;i<str.length();i++){
-            if(Character.isLetter(str.charAt(i))|| Character.isDigit(str.charAt(i))) s=s+str.charAt(i);
-        }
+    public boolean isPalindrome(String s) {
+        StringBuilder sb=new StringBuilder();
+        s=s.trim();
         s=s.toLowerCase();
+        if(s==null) return true;
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(Character.isDigit(ch)||Character.isLetter(ch)) sb.append(ch);
+        }
+        return checkPalindrome(sb.toString());
+    }
+    public boolean checkPalindrome(String s){
         int i=0;
         int j=s.length()-1;
-        while(i<=j){
+        while(i<j){
             if(s.charAt(i)!=s.charAt(j)) return false;
-            i++;
-            j--;
+            else{
+                i++;
+                j--;
+            }
         }
         return true;
     }
