2d193c59dff6057636f57775a83ebdb21d108962
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-09-21 23:04:31 +0530

    Time: 71 ms (22.52%), Space: 58.7 MB (5.01%) - LeetHub

---
+++ 0881-boats-to-save-people/0881-boats-to-save-people.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int numRescueBoats(int[] arr, int limit) {
+        ArrayList<Integer> list = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++)
+            list.add(arr[i]);
+
+        Collections.sort(list);
+
+        int i = 0;
+        int j = list.size() - 1;
+        int count = 0;
+
+        while (i < j) {
+            int sum = list.get(i) + list.get(j);
+
+            if (sum <= limit) {
+                list.set(i, -1);
+                list.set(j, -1);
+
+                i++;
+                j--;
+                count++;
+            } else {
+                j--;
+            }
+        }
+
+        for (int a = 0; a < list.size(); a++) {
+            if (list.get(a) != -1)
+                count++;
+        }
+
+        return count;
+    }
+}
