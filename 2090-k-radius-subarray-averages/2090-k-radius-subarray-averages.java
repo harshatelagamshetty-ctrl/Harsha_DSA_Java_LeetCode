75b28707f229ceedd65b9fdcbdb12d089c2b5fe0
Harsha Vardhan Telagamshetty <harshatelagamshetty@gmail.com>
2026-09-26 00:35:41 +0530

    Time: 11 ms (5.85%), Space: 175 MB (73.17%) - LeetHub

--- 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -1,28 +1,25 @@
 class Solution {
-    public int[] getAverages(int[] arr, int r) {
-        if(r==0) return arr;
-        if(2*r+1>arr.length) {
-        int[] res = new int[arr.length];
-        for(int i=0;i<res.length;i++) res[i]=-1;
-        return res;
-}
-        ArrayList<Integer> list=new ArrayList<>();
-        for(int i=1;i<=r;i++) list.add(-1);
+    public int[] getAverages(int[] arr, int k) {
+        if(arr.length<2*k+1){
+            int[] res=new int[arr.length];
+            for(int i=0;i<res.length;i++) res[i]=-1;
+            return res;
+        }
+        ArrayList<Integer>list=new ArrayList<>();
+        for(int i=1;i<=k;i++) list.add(-1);
         long sum=0;
-        for(int i=0;i<=2*r;i++){
+        for(int i=0;i<=2*k;i++) {
             sum=sum+arr[i];
         }
-        list.add((int)(sum/(2*r+1)));
-        for(int i=2*r+1;i<arr.length;i++){
-            sum=sum+arr[i]-arr[i-(2*r+1)];
-            list.add((int)(sum/(2*r+1)));
+        list.add((int)(sum/(2*k+1)));
+        for(int i=2*k+1;i<arr.length;i++){
+            sum=sum+arr[i];
+            sum=sum-arr[i-(2*k+1)];
+            list.add((int)(sum/(2*k+1)));
         }
-        while(list.size()!=arr.length) list.add(-1);
-        int[] res=new int[arr.length];
-        for(int i=0;i<arr.length;i++){
-            res[i]=list.get(i); 
-        }
+        for(int i=1;i<=k;i++) list.add(-1);
+        int[] res=new int[list.size()];
+        for(int i=0;i<res.length;i++) res[i]=list.get(i);
         return res;
-
     }
 }
